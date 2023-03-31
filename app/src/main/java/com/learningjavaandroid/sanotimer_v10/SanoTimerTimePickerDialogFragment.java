e801d5f978e1f0c37dcbdd46cf7d43a2290c4131
muzafferkanaan <mkanaan@sanotechcorp.com>
2023-04-01 00:41:51 +0300

    01.04.2023 - figured out a solution path to store start and stop time data: as strings in the form "HH:MM". See SanoTimerTimePickerDialogFragment.java.

--- app/src/main/java/com/learningjavaandroid/sanotimer_v10/SanoTimerTimePickerDialogFragment.java
+++ app/src/main/java/com/learningjavaandroid/sanotimer_v10/SanoTimerTimePickerDialogFragment.java
@@ -3,6 +3,7 @@ package com.learningjavaandroid.sanotimer_v10;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.widget.TimePicker;
@@ -31,5 +32,21 @@ public class SanoTimerTimePickerDialogFragment extends DialogFragment
         // 30.03.2023 - I BELIEVE this is where we will add the code to save the time to the database.
         Log.d("TIME_SET", "onTimeSet: " + " hour set: " + hourOfDay
                 + " minute set: " + minute);
+        // 31.03.2023 - more test code....to format hour and day as string in the form "HH:MM".
+        String setTime = hourOfDay + ":" + minute;
+        // 31.03.2023 - ok....now let's see if we can unpack the string to get hour and minute
+        // 01.04.2023 - first set the string splitter object with : as the delimiter
+        // TODO - 01.04.2023 - move this code to a simple utils method.
+        TextUtils.SimpleStringSplitter stringSplitter =
+                new TextUtils.SimpleStringSplitter(':');
+        // 01.04.2023 - set the text to split.
+        stringSplitter.setString(setTime);
+
+        // 01.04.2023 - now split!
+        // TODO 01.04.2023 - move this code to a simple utils method.
+        int testHour = Integer.parseInt(stringSplitter.next());
+        int testMinute = Integer.parseInt(stringSplitter.next());
+        Log.d("TIME_SET_2", "onTimeSet: " + " testHour: " + testHour
+                + " testMinute: " + testMinute);
     }
 }
